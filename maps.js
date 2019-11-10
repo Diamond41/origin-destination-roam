b1f720ff6202275e50c6ffd52b30773d252efb9e
Garry Derby <g_derby@hotmail.com>
2019-11-10 20:48:47 +0000

    Update maps.js

--- maps.js
+++ maps.js
@@ -1,63 +1,15 @@
-function initMap() {
-    
-var map = new google.maps.Map(document.getElementById("map"), {
-
-        zoom: 3,
-
-        center: {
-
-            lat: 46.619261,
-
-            lng: -33.134766
-
-        }
-
-    });
-
-
-
-    var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
-
-
-
-    var locations = [{
-
-        lat: 40.785091,
-
-        lng: -73.968285
-
-    }, {
-
-        lat: 41.084045,
-
-        lng: -73.874256
-
-    }, {
-
-        lat: 40.754932,
-
-        lng: -73.984016
-
-    }];
-
-
-
-    var markers = locations.map(function(location, i) {
-
-        return new google.maps.Marker({
-
-            position: location,
-
-            label: labels[i % labels.length]
-
-        });
-
-    });
-  
-  var markerCluster = new MarkerClusterer(map, markers, {
-
-        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-
-    });
-
-}
+function init_map()
+var myOptions = 
+    zoom:12,
+      center:new google.maps.LatLng(53.1659,-1.2612000000000307),
+      mapTypeId: google.maps.MapTypeId.ROADMAP};
+      map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
+      marker = new google.maps.Marker({map: map,position: 
+      new google.maps.LatLng(53.1659,-1.2612000000000307)});
+      infowindow = new google.maps.InfoWindow({content:
+     '<strong>Venice</strong>
+     <br>
+     del Bovolo, 4303<br>30124 Venezia VE Venezia<br>'});
+     google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});
+     infowindow.open(map,marker);}
+     google.maps.event.addDomListener(window, 'load', init_map);
